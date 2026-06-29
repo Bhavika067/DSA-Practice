6296fed48ad782683f03d2f6334c751e51868ffb
Bhavika Joon <bhavika.6215@gmail.com>
2026-06-29 15:44:11 +0530

    smallest_element.cpp
    Add program to find the smallest element in an array

---
+++ Arrays/smallest_element.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {5, 2, 9, 1, 7};
+    int n = 5;
+
+    int smallest = arr[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] < smallest)
+        {
+            smallest = arr[i];
+        }
+    }
+
+    cout << "Smallest Element: " << smallest;
+
+    return 0;
+}
